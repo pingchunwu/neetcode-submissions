aef5222d013897868e8589a2942190f6002fbc65
Billy <60102680+pingchunwu@users.noreply.github.com>
2026-07-28 22:44:34 +0800

    Add: partition-labels - submission-4

---
+++ Data Structures & Algorithms/partition-labels/submission-4.java
@@ -0,0 +1,59 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        Map<Character, Integer> map = new HashMap();
+        List<Integer> list = new ArrayList();
+        for (int i = 0 ; i < s.length(); i++) {
+            map.put(s.charAt(i), i);
+        }
+        List<Integer> res = new ArrayList();
+        int end = map.get(s.charAt(0));
+        int count = 0;
+        for (int i = 0 ; i < s.length(); i++) {
+            end = Math.max(map.get(s.charAt(i)), end);
+            count++;
+            if (end == i) {
+                res.add(count);
+                count = 0;
+            }
+        }
+
+        
+        return res;
+    }
+}
+
+// class Solution {
+//     public List<Integer> partitionLabels(String s) {
+//         Map<Character, Integer> map = new HashMap();
+//         List<Integer> list = new ArrayList();
+//         for (int i = 0 ; i < s.toCharArray().length; i++) {
+//             char c = s.charAt(i);
+//             if (map.get(c) == null) {
+//                 map.put(c, i);
+//                 list.add(i);
+//             } else {
+//                 int prev = map.get(c);
+//                 for (int j = prev ; j < i; j++) {
+//                     map.put(s.charAt(j), prev);
+//                     list.set(j, prev);
+//                 }
+//                 list.add(prev);
+//             }
+//         }
+//         int cur = list.get(0);
+//         int count = 0;
+//         List<Integer> res = new ArrayList();
+//         for (int i : list) {
+//             if (cur != i) {
+//                 res.add(count);
+//                 cur = i;
+//                 count = 0;
+//             }
+//             count++;
+//         }
+
+//         res.add(count);
+        
+//         return res;
+//     }
+// }
