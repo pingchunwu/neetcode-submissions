63ed5ad48167f45818948110a0f0c3234c1e4d56
Billy <60102680+pingchunwu@users.noreply.github.com>
2026-07-29 22:08:07 +0800

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int count = 0;
+        Stack<Character> stack = new Stack();
+        for (int i = 0 ; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '*') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')') {
+                if (stack.size() == 0) {
+                    return false;
+                }
+                boolean hasLeft = false;
+                for (int j = stack.size() - 1 ; j >= 0; j--) {
+                    if (stack.get(j) == '(') {
+                        stack.remove(j);
+                        hasLeft = true;
+                        break;
+                    }
+                }
+                if (hasLeft) {
+                    continue;
+                }
+                stack.pop();
+            }
+        }
+        int right = 0;
+        while(stack.size() > 0) {
+            char c = stack.pop();
+            if (c == '*') {
+                right++;
+            } else if (c == '(') {
+                if (right > 0) {
+                    right--;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
