dadc21dd22b6527534d677da66d7794c1aa4f1ce
Billy <60102680+pingchunwu@users.noreply.github.com>
2026-09-21 15:10:06 +0800

    Add: reverse-integer - submission-6

---
+++ Data Structures & Algorithms/reverse-integer/submission-6.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int reverse(int x) {
+        int cur = x;
+        int res = 0;
+        int tens = 0;
+        try {
+            while (Math.absExact(cur) / 10 > 0) {
+                int left = cur % 10;
+                cur /= 10;
+                res *= 10;
+                res += left;
+                if (++tens == 9) {
+                    return rest(cur, res, x);
+                }
+            }
+        } catch (Exception e) {
+            return 0;
+        }
+        
+        res *= 10;
+        res += cur;
+
+        return res;
+    }
+
+    private int rest(int cur, int res, int x) {
+        if (res > 0) {
+            if(res > Integer.MAX_VALUE / 10) {
+                return 0;
+            } else if (cur > 10) {
+                return 0;
+            } else if (res * 10 + cur < 0) {
+                return 0;
+            }
+            return res * 10 + cur;
+        } else {
+            if(res < Integer.MIN_VALUE / 10) {
+                return 0;
+            } else if (Math.abs(cur) > 10) {
+                return 0;
+            } else if (res * 10 + cur > 0) {
+                return 0;
+            }
+            return res * 10 + cur;
+        }
+    }
+}
